377f9c805550c2e99dd4c702c12aaf6960bf8252
Akash7218 <64300977+Akash7218@users.noreply.github.com>
2020-04-24 23:47:29 -0700

    js

---
+++ resources/js/main.js
@@ -0,0 +1,38 @@
+function openNav() {
+    document.getElementById("myNav").style.width = "100%"
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%"
+}
+$(document).ready(function() {
+    mixitup(".tab")
+}), $(document).ready(function() {
+    $(".js--services-section").waypoint(function(t) {
+        "down" == t ? $("nav").addClass("sticky") : $("nav").removeClass("sticky")
+    })
+});
+$("svg.radial-progress").each(function(e, t) {
+    $(this).find($("circle.complete")).removeAttr("style")
+}), $(window).scroll(function() {
+    $("svg.radial-progress").each(function(e, t) {
+        $(window).scrollTop() > $(this).offset().top - .75 * $(window).height() && $(window).scrollTop() < $(this).offset().top + $(this).height() - .25 * $(window).height() && (percent = $(t).data("percentage"), radius = $(this).find($("circle.complete")).attr("r"), circumference = 2 * Math.PI * radius, strokeDashOffset = circumference - percent * circumference / 100, $(this).find($("circle.complete")).animate({
+            "stroke-dashoffset": strokeDashOffset
+        }, 1250))
+    })
+}).trigger("scroll");
+
+
+ $(document).ready(function(){
+      
+                    
+      //MIXITUP (Portfolio Section)
+      var mixer = mixitup('.myself' ) ;
+      
+      
+      
+      
+      
+      
+            
+     }) ;
